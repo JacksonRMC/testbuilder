d1f8277bbf738bbb5da8052618958e36bf841a38
Jackson Carter <jcarter0803@gmail.com>
2017-03-16 20:06:03 -0700

    made parser work for amex and diners club

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,11 +8,12 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+	var firstDigits = parseInt(cardNumber.slice(0,2));
+	if((firstDigits == 38 || firstDigits == 39) && cardNumber.length === 14){
+		return "Diner's Club";
+	} else if((firstDigits == 34 || firstDigits == 37) && cardNumber.length === 15){
+		return "American Express";
+	} 
 };
 
 
